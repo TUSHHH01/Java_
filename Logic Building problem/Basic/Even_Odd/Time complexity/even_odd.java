18ef8db0c281f82feab83a95849e737d6d848c47
Omkar <omkarsjagadale54@gmail.com>
2026-06-27 23:57:17 +0530

    Start of Dsa jounery

---
+++ Logic Building problem/Basic/Even_Odd/Time complexity/even_odd.java
@@ -0,0 +1,25 @@
+public class even_odd {
+    public static boolean checkeven_odd(int n)
+    {
+        if((n&1)==0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public static void main(String args[])
+    {
+        int num = 7;
+        if(checkeven_odd(num))
+        {
+            System.out.println("true");
+        }
+        else
+        {
+            System.out.println("False");
+        }
+    }
+}
